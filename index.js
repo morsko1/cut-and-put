fc73b8719aa29bcf0ee593af08aa34b1a82b703f
morsko1 <morsko1@yandex.ru>
2019-08-09 20:29:37 +0300

    improved functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const gapSize = 20
 let counterX = 0;
 let counterY = 0;
 let diameter = 6 * 43;
+let isCircle = true;
 let numOfImagesInRow = Math.floor((904 - xIndent) / (diameter + gapSize));
 
 const can = document.getElementById('can').getContext('2d');
@@ -26,6 +27,10 @@ const onDiameterInput = (event) => {
     diameter = parseFloat(event.target.value) * 43;
     numOfImagesInRow = Math.floor((904 - xIndent) / (diameter + gapSize));
 };
+
+const onCircleChange = (event) => {
+    isCircle = event.target.checked;
+}
 
 const clearCan = () => {
     counterX = counterY = 0;
@@ -49,13 +54,38 @@ const addImageToList = () => {
     const isImgHorizontal = whRatio > 1;
     const hdRatio = image.height / diameter;
     const wdRatio = image.width / diameter;
+
+    if (isCircle) {
+        // push current state into canvas
+        // for ability of multiple insert
+        can.save();
+        can.beginPath();
+        can.arc(
+            diameter/2 + xIndent + diameter*counterX + gapX*counterX + (!isImgHorizontal ? (diameter - image.width/hdRatio) / 2 : 0),
+            diameter/2 + yIndent + diameter*counterY + gapY*counterY,
+            isImgHorizontal ? diameter*hwRatio > diameter ? diameter*whRatio/2 : diameter/2 : diameter*hwRatio > diameter ? diameter/2 : diameter*hwRatio/2,
+            0,
+            Math.PI * 2,
+            false
+        );
+        can.clip(); 
+        can.stroke();
+        can.closePath();
+    }
+
     can.drawImage(
         image,
-        xIndent + diameter*counterX + gapX*counterX + (!isImgHorizontal ? (diameter - image.width/hdRatio) / 2 : 0),
-        yIndent + diameter*counterY + gapY*counterY + (isImgHorizontal ? (diameter - image.height/wdRatio) / 2 : 0),
+        xIndent + diameter*counterX + gapX*counterX + (!isImgHorizontal ? (diameter - image.width/hdRatio) : 0),
+        yIndent + diameter*counterY + gapY*counterY + (isImgHorizontal ? (diameter - image.height/wdRatio)/2 : 0),
         diameter*hwRatio > diameter ? diameter*whRatio : diameter,
         diameter*hwRatio > diameter ? diameter : diameter*hwRatio
     );
+
+    if (isCircle) {
+        // restore can context to the state
+        can.restore();
+    }
+
     ++counterX;
     if (counterX === numOfImagesInRow) {
         counterX = 0;
